dfde173f0b43702c167c9aef74edd4a4e7f1afd9
Gopikakaruppaiah <gopikakaruppaiah15@gmail.com>
2026-06-05 17:48:15 +0530

    Time: 3 ms (100%), Space: 50.6 MB (10.87%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -10,27 +10,27 @@
  */
 class Solution {
     public int[] nextLargerNodes(ListNode head) {
-        int count = 0;
+        int n = 0;
         ListNode temp = head;
         while (temp != null) {
-            count++;
+            n++;
             temp = temp.next;
         }
-        int[] ans = new int[count];
+        int[] values = new int[n];
         temp = head;
-        int i = 0;
-        while (temp != null) {
-            ListNode curr = temp.next;
-            while (curr != null && curr.val <= temp.val) {
-                curr = curr.next;
-            }
-            if (curr != null) {
-                ans[i] = curr.val;
-            } else {
-                ans[i] = 0;
-            }
+        for (int i = 0; i < n; i++) {
+            values[i] = temp.val;
             temp = temp.next;
-            i++;
+        }
+        int[] ans = new int[n];
+        int[] stack = new int[n];
+        int top = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            while (top != -1 && stack[top] <= values[i]) {
+                top--;
+            }
+            ans[i] = (top == -1) ? 0 : stack[top];
+            stack[++top] = values[i];
         }
         return ans;
     }
